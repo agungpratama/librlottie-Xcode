64408f1b0646a27c38eb01b276e8c679eeb75be4
Agung Pratama Putra <agung1991pratama@yahoo.co.id>
2020-11-23 10:41:38 +0700

    Update config.h

--- generate/config.h
+++ generate/config.h
@@ -17,7 +17,15 @@
 // enable module building of image loader
 //#define LOTTIE_IMAGE_MODULE_SUPPORT
 
+
+//Disabling cache support to fix the bug that happens when using librlottie with SDWebImage/SDWebImageLottieCoder
+//credit to stasmilke's reply
+/*
+it was caused by passing empty string in lottie_animation_from_data (SDImageLottieCoder.m). you should pass unique value as second argument, because it's used as cache key (or you can disable LOTTIE_CACHE_SUPPORT in librlottie config.h file)
+*/
+//issue thread: https://github.com/SDWebImage/SDWebImageLottieCoder/issues/4
+
 // enable lottie model caching
-#define LOTTIE_CACHE_SUPPORT
+//#define LOTTIE_CACHE_SUPPORT
 
 #endif  // CONFIG_H
